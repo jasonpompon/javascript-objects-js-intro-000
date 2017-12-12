7515fa86e75523229ea0ccbbdc6ec51d4ffca604
Jason Pompon <jasonpompon@gmail.com>
2017-12-12 00:47:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {
     MJ: "Thriller",
     Pink: "Kiss roses"
 }
+
+var updatePlaylist = (playlist, artistName, songTitle) {
+    return Object.assign({}, playlist, {artistName: songTitle})
+}
